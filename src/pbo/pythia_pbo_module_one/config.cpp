02eee188dd7b48fdbe13e80daa4319ca9901e696
Lukasz Taczuk <overfl0@users.noreply.github.com>
2017-12-23 21:54:23 +0100

    Added sample pbo for testing

---
+++ src/pbo/pythia_pbo_module_one/config.cpp
@@ -0,0 +1,8 @@
+class CfgPatches {
+    class Pythia_Sample_Module_One {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.66;
+        requiredAddons[] = {};
+    };
+};
---
+++ src/pbo/pythia_pbo_something_else/config.cpp
@@ -0,0 +1,8 @@
+class CfgPatches {
+    class Pythia_Sample_Something_Else {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.66;
+        requiredAddons[] = {};
+    };
+};
